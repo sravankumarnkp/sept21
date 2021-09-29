29ac502e5267850ef17ff7ca4a0f36d8d8585aee
sravankumarnkp <sravankumarnkp@gmail.com>
2021-09-30 01:27:00 +1000

    Design pattern model

---
+++ firstautotest/Tests/TM_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using firstautotest.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace firstautotest
+{
+	class TM_Tests
+	{
+		static void Main(string[] args)
+		{
+			//open the browser and maximise
+			IWebDriver driver = new ChromeDriver();
+
+			//create login page
+			LoginPage lpobj = new LoginPage();
+			lpobj.gotoLoginPage(driver);
+
+			//create home page
+			HomePage Hpobj = new HomePage();
+			Hpobj.gotoHomePage(driver);
+			//tm page
+			TMPage tmobj = new TMPage();
+			tmobj.createTM(driver);
+			tmobj.editTM(driver);
+			tmobj.deleteTM(driver);
+			
+		}
+	}
+}
---
+++ firstautotest/Utlities/Wait.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace firstautotest.Utlities
+{
+    class Wait
+    {
+        public static void WaitForElementToBeClickable(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+
+            if (locatorType == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locatorValue)));
+            }
+
+            if (locatorType == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(locatorValue)));
+            }
+
+            if (locatorType == "CSSSelector")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(locatorValue)));
+            }
+
+        }
+    }
+}
